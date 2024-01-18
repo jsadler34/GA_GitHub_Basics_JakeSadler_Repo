fcd16c66b7e44042b1ab1d3de3b4ea5fd55ffa05
jsadler@student.rtc.edu <jsadler@stuent.rtc.edu>
2024-01-17 19:58:52 -0800

    Adding my name

---
+++ GA_GitHub_Basics_Repo_JakeSadler/GA_GitHub_Basics_JakeSadler/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace GA_GitHub_Basics_JakeSadler
+{        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, Professor!");
+            Console.WriteLine("Jake Sadler - I would like to learn how to build programs!")
+
+
+        }
+    }
+}
